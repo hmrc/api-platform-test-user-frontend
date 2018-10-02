bcefca1afa7ccaec5fdd3710605179e18eaacd42
Gurpreet Bhamra <gurpreet.bhamra@digital.hmrc.gov.uk>
2018-10-02 11:12:19 +0100

    APIS-3656 - Create test user FE content changes

--- test/it/uk/gov/hmrc/testuser/pages/CreateTestUserPage.scala
+++ test/it/uk/gov/hmrc/testuser/pages/CreateTestUserPage.scala
@@ -23,7 +23,7 @@ object CreateTestUserPage extends WebPage {
 
   override val url: String = "http://localhost:6001/api-test-user"
 
-  override def isCurrentPage: Boolean = find(cssSelector("h1")).fold(false)(_.text == "Create test user")
+  override def isCurrentPage: Boolean = find(cssSelector("h1")).fold(false)(_.text == "Create a test user")
 
   val individualCheckbox: By = By.id("Individual")
 
