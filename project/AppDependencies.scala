3954d67fb18ceb97de18a7863acd7e8d7c567faa
John Green <john.green@digital.hmrc.gov.uk>
2023-07-20 12:35:26 +0100

    API-7020: Improve Content Security Policy using latest play-bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,14 +10,14 @@ object AppDependencies {
   val seleniumVersion = "4.2.2"
 
   lazy val scope: String = "test, it"
-  lazy val bootStrapVersion = "7.15.0"
+  lazy val bootStrapVersion = "7.19.0"
 
   lazy  val compile = Seq(
     ws,
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % bootStrapVersion,
     "uk.gov.hmrc"             %% "play-partials"                % "8.4.0-play-28",
     "uk.gov.hmrc"             %% "domain"                       % "8.1.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"           % "7.3.0-play-28"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"           % "7.14.0-play-28"
   )
 
   lazy val test = Seq(
