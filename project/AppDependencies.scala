21934dd49c9fd0a6950d7d87159e0e5deb2b335e
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-01-29 12:13:35 +0000

    Bobbins - wip

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,29 +7,32 @@ object AppDependencies {
 
   lazy val appDependencies: Seq[ModuleID] = compile ++ test
   
-  val seleniumVersion = "4.2.2"
-
+  
   lazy val scope: String = "test, it"
-  lazy val bootStrapVersion = "7.19.0"
+  lazy val bootStrapVersion = "8.4.0"
+  // val seleniumVersion = "4.2.2"
+  lazy val seleniumVersion = "4.14.0"
 
   lazy  val compile = Seq(
     ws,
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % bootStrapVersion,
-    "uk.gov.hmrc"             %% "play-partials"                % "8.4.0-play-28",
-    "uk.gov.hmrc"             %% "domain"                       % "8.1.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"           % "7.14.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"   % bootStrapVersion,
+    "uk.gov.hmrc"             %% "play-partials-play-30"        % "9.1.0",
+    "uk.gov.hmrc"             %% "domain-play-30"               % "9.0.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "8.4.0"
   )
 
   lazy val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"       % bootStrapVersion,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"       % bootStrapVersion,
     "org.jsoup"               %  "jsoup"                        % "1.8.1",
-    "com.github.tomakehurst"  %  "wiremock-jre8-standalone"     % "2.31.0",
-    "org.scalatestplus"       %% "selenium-4-2"                 % "3.2.13.0",
-    "org.seleniumhq.selenium" %  "selenium-api"                 % seleniumVersion,
-    "org.seleniumhq.selenium" %  "selenium-firefox-driver"      % seleniumVersion,
-    "org.seleniumhq.selenium" %  "selenium-chrome-driver"       % seleniumVersion,
+    // "com.github.tomakehurst"  %  "wiremock-jre8-standalone"     % "2.31.0",
+    // "org.scalatestplus"       %% "selenium-4-2"                 % "3.2.13.0",
+    // "org.seleniumhq.selenium" %  "selenium-api"                 % seleniumVersion,
+    // "org.seleniumhq.selenium" %  "selenium-firefox-driver"      % seleniumVersion,
+    // "org.seleniumhq.selenium" %  "selenium-chrome-driver"       % seleniumVersion,
     "org.mockito"             %% "mockito-scala-scalatest"      % "1.17.29",
-    "org.scalatest"           %% "scalatest"                    % "3.2.17",
-    "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.2"
+    // "org.scalatest"           %% "scalatest"                    % "3.2.17",
+    // "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.2",
+    "org.seleniumhq.selenium" % "selenium-java"                 % "4.17.0",
+    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.10.0"
   ).map(_ % scope)
 }
