231eb55923efb0d8b3941ad21707879b28bee3ed
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-02-06 10:28:18 +0000

    ui-test-runner-example - format and reset sassify

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,16 +23,8 @@ object AppDependencies {
   lazy val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"       % bootStrapVersion,
     "org.jsoup"               %  "jsoup"                        % "1.8.1",
-    // "com.github.tomakehurst"  %  "wiremock-jre8-standalone"     % "2.31.0",
-    // "org.scalatestplus"       %% "selenium-4-2"                 % "3.2.13.0",
-    // "org.seleniumhq.selenium" %  "selenium-api"                 % seleniumVersion,
-    // "org.seleniumhq.selenium" %  "selenium-firefox-driver"      % seleniumVersion,
-    // "org.seleniumhq.selenium" %  "selenium-chrome-driver"       % seleniumVersion,
     "org.mockito"             %% "mockito-scala-scalatest"      % "1.17.29",
-    // "org.scalatest"           %% "scalatest"                    % "3.2.17",
-    // "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.2",
-//    "org.seleniumhq.selenium" % "selenium-java"                 % "4.17.0",
-    "com.titusfortner"        % "selenium-logger"               % "2.3.0",
-     "uk.gov.hmrc"             %% "ui-test-runner"               % "0.16.0"
+    "com.titusfortner"        %  "selenium-logger"              % "2.3.0",
+     "uk.gov.hmrc"            %% "ui-test-runner"               % "0.16.0"
   ).map(_ % scope)
 }
