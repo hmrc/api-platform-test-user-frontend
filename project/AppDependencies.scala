f3d81984ae50f2c0411f9009eeb66fef18200d0e
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-03-17 12:16:05 +0000

    API-8301 - Bobby warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import play.sbt.PlayImport._
 object AppDependencies {
   def apply() = compile ++ test
   
-  lazy val bootStrapVersion = "9.7.0"
+  lazy val bootStrapVersion = "9.11.0"
   lazy val seleniumVersion = "4.14.1"
 
   lazy  val compile = Seq(
@@ -21,8 +21,8 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"       % bootStrapVersion,
     "org.jsoup"               %  "jsoup"                        % "1.8.1",
     "org.mockito"             %% "mockito-scala-scalatest"      % "1.17.30",
-    "com.titusfortner"        %  "selenium-logger"              % "2.3.0",
-    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.32.0"
+    // "com.titusfortner"        %  "selenium-logger"              % "2.3.0",
+    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.45.0"
   ).map(_ % "test")
 }
 
