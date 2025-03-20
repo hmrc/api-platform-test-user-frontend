f0e091fd7b866259a7804fae2378c1d602b05979
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-03-20 14:55:31 +0000

    API-8301 - Bobby warnings (#136)
    * API-8301 - Bobby warnings
    
    * API-8301 - PR Bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import play.sbt.PlayImport._
 object AppDependencies {
   def apply() = compile ++ test
   
-  lazy val bootStrapVersion = "9.7.0"
+  lazy val bootStrapVersion = "9.11.0"
   lazy val seleniumVersion = "4.14.1"
 
   lazy  val compile = Seq(
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"   % bootStrapVersion,
     "uk.gov.hmrc"             %% "play-partials-play-30"        % "10.0.0",
     "uk.gov.hmrc"             %% "domain-play-30"               % "10.0.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "11.11.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "11.12.0",
     "org.typelevel"           %% "cats-core"                    % "2.10.0"
 
   )
@@ -21,8 +21,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"       % bootStrapVersion,
     "org.jsoup"               %  "jsoup"                        % "1.8.1",
     "org.mockito"             %% "mockito-scala-scalatest"      % "1.17.30",
-    "com.titusfortner"        %  "selenium-logger"              % "2.3.0",
-    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.32.0"
+    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.45.0"
   ).map(_ % "test")
 }
 
