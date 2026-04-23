9a1c01f334f8b77408512183f56b0bf0531608e0
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2026-04-23 13:58:09 +0100

    API-9121 - PR bot fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
   lazy  val compile = Seq(
     ws,
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"   % bootStrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "12.25.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "12.32.0",
     "org.typelevel"           %% "cats-core"                    % "2.10.0"
 
   )
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"       % bootStrapVersion,
     "org.jsoup"               %  "jsoup"                        % "1.8.1",
     "org.mockito"             %% "mockito-scala-scalatest"      % "1.17.30",
-    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.46.0"
+    "uk.gov.hmrc"             %% "ui-test-runner"               % "0.54.0"
   ).map(_ % "test")
 }
 
